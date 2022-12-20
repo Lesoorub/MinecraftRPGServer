65b490c7a0c98e080604aa714fbb26c01a048649
Andrew <69423536+Lesoorub@users.noreply.github.com>
2022-12-21 01:31:39 +0300

    Sync

--- MinecraftRPGServer/core/World/BlockState.cs
+++ MinecraftRPGServer/core/World/BlockState.cs
@@ -1,16 +1,23 @@
 ﻿using System.Linq;
+using MinecraftData._1_18_2;
+using MinecraftData._1_18_2.blocks.minecraft;
 
 public struct BlockState
 {
     public short StateID;
     public BlockNameID id { get => GlobalPalette.GetNameID(StateID); }
     public bool solid { get => StateID > 0; }
-    public bool haveCollision 
-    { 
-        get => 
-            BlockAttribute.blocks.TryGetValue(defaultBlockState, out var iWorldBlock) 
-            ? iWorldBlock.hasCollision :
-            StateID != 0; 
+    public IBlockData iBlockData =>
+        GlobalPalette.GetBlockData(StateID);
+        //BlockAttribute.blocks.TryGetValue(defaultBlockState, out var iWorldBlock) ? (iWorldBlock, false) : (default, true);
+    public bool haveCollision
+    {
+        get
+        {
+            var block = iBlockData;
+            var collisionShape = block.States.First(x => x.Id == block.DefaultStateID).CollisionShape;
+            return collisionShape.HasValue || collisionShape.Value == 0;
+        }
     }
     public DefaultBlockState defaultBlockState => GlobalPalette.GetDefaultStateID(StateID);
     public float hardness => BlockAttribute.blocks.TryGetValue(defaultBlockState, out var v) ? v.hardness : -1;
--- MinecraftRPGServer/core/World/World.cs
+++ MinecraftRPGServer/core/World/World.cs
@@ -62,6 +62,9 @@ public abstract class World : ICollisionProvider, ILightWorld
     public bool IsLightTransparent(int ax, short ay, int az)
     {
         var block = GetBlock(ax, ay, az);
+        var data = block.iBlockData;
+        return data.IsTransparent;
+        //GlobalPalette.GetBlockData(GlobalPalette.GetNameID(block.StateID));
         if (block == DefaultBlockState.air) return true;
         if (block == DefaultBlockState.dead_bush) return true;
         if (block == DefaultBlockState.grass) return true;
